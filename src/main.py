22ed046a3aab4a1570edf660b52ba2aa0fb49917
alexwitt23 <awitt2399@utexas.edu>
2021-04-22 12:03:17 -0500

    add training files

--- src/main.py
+++ src/main.py
@@ -1,18 +1,49 @@
-
 import cv2
 import torch
+import numpy as np
 
-import detector
-
-
-image = cv2.imread("/home/alex/datasets/coco/images/val2017/000000000285.jpg")
-image = cv2.resize(image, (512, 512))
+from src.model import detector
 
 
-model = detector.Detector(num_classes=10, confidence=.01)
+def normalize(
+    img, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225), max_pixel_value=255.0,
+) -> np.ndarray:
 
+    mean = np.array(mean, dtype=np.float32)
+    mean *= max_pixel_value
+
+    std = np.array(std, dtype=np.float32)
+    std *= max_pixel_value
+
+    denominator = np.reciprocal(std, dtype=np.float32)
+
+    img = img.astype(np.float32)
+    img -= mean
+    img *= denominator
+
+    return img
+
+
+save_path = "/tmp/img.jpg"
+image = cv2.imread(
+    "/home/alex/Desktop/projects/minimal-object-detector/src/train/data/images/2020-Toyota-86-GT-TRD-Wheels.jpg"
+)
+image_ori = cv2.resize(image, (512, 512))
+image = normalize(image_ori)
+
+
+model = detector.Detector(timestamp="2021-04-22T11.25.25")
+model.eval()
 
 with torch.no_grad():
-    boxes = model(torch.Tensor(image).permute(2, 0, 1).unsqueeze(0))
+    image = torch.Tensor(image)
+    if torch.cuda.is_available():
+        image = image.cuda()
+    boxes = model.get_boxes(image.permute(2, 0, 1).unsqueeze(0))
 
-print(boxes)
+for box in boxes[0]:
+    print(box)
+    box = (box.box * torch.Tensor([512] * 4)).int().tolist()
+    cv2.rectangle(image_ori, (box[0], box[1]), (box[2], box[3]), (255, 0, 0), 2)
+
+cv2.imwrite(save_path, image_ori)
